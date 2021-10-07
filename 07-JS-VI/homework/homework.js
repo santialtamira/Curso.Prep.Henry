f56ed405c42d8408cbd0e4d43a75d82cc262387f
santiago altamira <santiagoaltamira@gmail.com>
2021-10-07 17:05:19 -0300

    finished all JSVI excercises

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,28 @@
 // Do not change any of the function names
 
 function mayuscula(nombre) {
-  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
+  //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en 
+  //mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var firstLetter = nombre[0].toUpperCase();
+  var othersLetters = nombre.slice(1);
+  var capitalized = firstLetter + othersLetters
+  return capitalized;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
-  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
+  //Vamos a recibir una función que realiza una operación matemática como callback junto con dos 
+  //números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +30,34 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var result = numeros.reduce(function (acc, currentValue){
+    return acc + currentValue;
+  });
+  cb(result);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb);
 }
 
 function map(array, cb) {
   // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` 
+  //en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newArr = array.map(cb);
+  return newArr;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  return array.filter((word) => word[0] == "a");
 }
 
 // No modificar nada debajo de esta línea
